26d876b0cc282467f1b9e3c8184c7fcffff97132
Roman Rudkouski <rudkouski.web@gmail.com>
2025-01-08 21:32:48 +0300

    Adding the Exo 2 font and applying it to the logo

--- src/app/Components/Header/index.tsx
+++ src/app/Components/Header/index.tsx
@@ -2,16 +2,16 @@ import styles from "../../Styles/header.module.scss";
 import HeaderBtn from "./Button Menu"
 
 export default function Header() {
-return (
-<div className={styles.header}>
-    <div className={styles.header_logo}>
-    Logo
-    </div>
-    <div className={styles.header_menu}>
-    <HeaderBtn name="Режимы"/>
-    <HeaderBtn name="Правила"/>
-    <HeaderBtn name="Контакты"/>
-    </div>
-</div>
-)
+    return (
+        <div className={styles.header}>
+            <div className={styles.header_logo}>
+                CROCGAME
+            </div>
+            <div className={styles.header_menu}>
+                <HeaderBtn name="Режимы" />
+                <HeaderBtn name="Правила" />
+                <HeaderBtn name="Контакты" />
+            </div>
+        </div>
+    )
 }

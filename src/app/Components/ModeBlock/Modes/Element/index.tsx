524982d76a861b232ca3e01808f55475cd259989
Roman Rudkouski <rudkouski.web@gmail.com>
2025-01-13 02:53:21 +0300

    Creating an array of objects with information about modes, reading from the array

--- src/app/Components/ModeBlock/Modes/Element/index.tsx
+++ src/app/Components/ModeBlock/Modes/Element/index.tsx
@@ -1,14 +1,16 @@
 import styles from "../../../../Styles/modeBlock.module.scss"
 
-export default function ElementModes() {
+export default function ElementModes(props:any) {
     return (
             <div className={styles.modeBlock_modes_element}>
                 <div className={styles.modeBlock_modes_element_header}>
-                        <div>age</div>
+                        <div>{props.age}</div>
                         <div>???</div>
                 </div>
-                <div className={styles.modeBlock_modes_element_icon}>icon</div>
-                <div className={styles.modeBlock_modes_element_name}>name</div>
+                <div className={styles.modeBlock_modes_element_icon}>
+                        <img src={props.icon} alt="" />
+                </div>
+                <div className={styles.modeBlock_modes_element_name}>{props.name}</div>
                 <div className={styles.modeBlock_modes_element_btn}>button</div>
             </div>
     );
--- src/app/Components/ModeBlock/Modes/index.tsx
+++ src/app/Components/ModeBlock/Modes/index.tsx
@@ -1,18 +1,13 @@
 import styles from "../../../Styles/modeBlock.module.scss"
 import ElementModes from "./Element";
+import modes from '../../../assets/modes.json'
 
 export default function Modes() {
   return (
     <div className={styles.modeBlock_modes}>
-      <ElementModes />
-      <ElementModes />
-      <ElementModes />
-      <ElementModes />
-      <ElementModes />
-      <ElementModes />
-      <ElementModes />
-      <ElementModes />
-      <ElementModes/>
+      {modes.map((el,i) => (
+        <ElementModes age={el.age} name={el.name} icon={el.icon} key={i}/>
+      ))}
     </div>
   );
 }

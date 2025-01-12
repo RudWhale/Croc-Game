fde97e95175984d4cb26f32da50741170196c2b9
Roman Rudkouski <rudkouski.web@gmail.com>
2025-01-13 02:09:27 +0300

    Creating sub-blocks and placing them

--- src/app/Components/ModeBlock/Modes/Element/index.tsx
+++ src/app/Components/ModeBlock/Modes/Element/index.tsx
@@ -2,6 +2,14 @@ import styles from "../../../../Styles/modeBlock.module.scss"
 
 export default function ElementModes() {
     return (
-            <div className={styles.modeBlock_modes_element}>1</div>
+            <div className={styles.modeBlock_modes_element}>
+                <div className={styles.modeBlock_modes_element_header}>
+                        <div>age</div>
+                        <div>???</div>
+                </div>
+                <div className={styles.modeBlock_modes_element_icon}>icon</div>
+                <div className={styles.modeBlock_modes_element_name}>name</div>
+                <div className={styles.modeBlock_modes_element_btn}>button</div>
+            </div>
     );
   }

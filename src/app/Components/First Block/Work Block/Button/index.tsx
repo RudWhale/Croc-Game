13ef90b63293c3f850569a1fa45f0d69448569fd
Roman Rudkouski <rudkouski.web@gmail.com>
2025-01-12 03:25:31 +0300

    Button styling and its animation on hovering

--- src/app/Components/First Block/Work Block/Button/index.tsx
+++ src/app/Components/First Block/Work Block/Button/index.tsx
@@ -2,6 +2,6 @@ import styles from "../../../../Styles/firstBlock.module.scss"
 
 export default function ButtonWB() {
     return (
-        <div className={styles.firstBlock_workBlock_btn}>кнопка</div>
+        <div className={styles.firstBlock_workBlock_btn}>ВЫБРАТЬ РЕЖИМ</div>
     );
 }

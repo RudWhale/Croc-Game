5c1caa6653b2fd5dc73fa57fedd9c727a7073b88
Roman Rudkouski <rudkouski.web@gmail.com>
2025-01-16 03:28:55 +0300

    Button navigation in FirstBlock with smooth transition to the block

--- src/app/Components/First Block/Work Block/Button/index.tsx
+++ src/app/Components/First Block/Work Block/Button/index.tsx
@@ -1,7 +1,15 @@
+"use client"
 import styles from "../../../../Styles/firstBlock.module.scss"
 
 export default function ButtonWB() {
+
+    const toGO:any = () => {
+        const BlockToGo: any = document.getElementById("modes");
+    
+        BlockToGo.scrollIntoView({ block: "center", behavior: "smooth" });
+      }
+
     return (
-        <div className={styles.firstBlock_workBlock_btn}>ВЫБРАТЬ РЕЖИМ</div>
+        <div className={styles.firstBlock_workBlock_btn} onClick={toGO} >ВЫБРАТЬ РЕЖИМ</div>
     );
 }
